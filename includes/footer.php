a47ef7ee64ffbc0acd70f0d76499525ea0ced48c
root <abdkrimoajkali@gmail.com>
2020-04-10 18:05:20 +0100

    adding footer to index
    new structer for  footer
    added a white logo in footer
    added go to top button
    new css styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,38 +1,200 @@
-<div class="footer-dark" style="height: 400px;">
-        <footer style="margin: 50px 0 0 0;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-3 item"><img src="assets/img/ENSEM-270x300.png"
-                            style="height: auto;width: 60%;"></div>
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>Contact</h3>
-                        <ul>
-                            <li><i class="fas fa-phone icon" style="color: #e42084;"></i><a href="#">&nbsp;Tél :
-                                    0522230789 &nbsp;</a></li>
-                            <li><i class="icon ion-ios-information-outline icon" style="color: #e42084;"></i><a
-                                    href="#">&nbsp;Fax: 0522231299</a></li>
-                            <li><i class="fas fa-envelope icon" style="color: #e42084;"></i><a href="#">&nbsp; Email:
-                                    contact@ensem.ac.ma</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="card card-cascade narrower">
-                            <div class="view view-cascade gradient-card-header peach-gradient"></div>
-                            <div class="card-body card-body-cascade text-center">
-                                <div id="map-container-google-9" class="z-depth-1-half map-container-5"
-                                    style="height: 300px"><iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13301.900179068509!2d-7.6570737!3d33.5410318!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x65b786162ba01eb0!2s%C3%89cole%20nationale%20sup%C3%A9rieure%20d&#39;%C3%A9lectricit%C3%A9%20et%20m%C3%A9canique!5e0!3m2!1sfr!2sma!4v1582198256879!5m2!1sfr!2sma"
-                                        width="100%" height="100%" frameborder="0" style="border:0"
-                                        allowfullscreen></iframe></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i
-                                class="icon ion-social-twitter"></i></a><a href="#"><i
-                                class="icon ion-social-snapchat"></i></a><a href="#"><i
-                                class="icon ion-social-instagram"></i></a></div>
-                </div>
-                <p class="copyright">ENSEM © 2020</p>
-            </div>
-        </footer>
-    </div>
+
+
+
+
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<div class="container">
+
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_half">
+
+						<div class="widget clearfix">
+
+							<img src="includes/images/footer-widget-logo.png" alt="" class="footer-logo">
+
+							<p>L'ENSEM de Casablanca est une école d'<strong>ingénieurs</strong> qui a pour mission de préparer les étudiants-ingénieurs aux métiers du secteur industriel.</p>
+
+							<div class="clearfix" style="padding: 10px 0;">
+								<div class="col_half">
+									<address class="nobottommargin">
+										<abbr title="Headquarters" style="display: inline-block;margin-bottom: 7px;"><strong>Localisation:</strong></abbr><br>
+										Route d’El Jadida, km 7<br>
+										BP  8118, Oasis ,Casablanca Maroc<br>
+									</address>
+
+								</div>
+								<div class="col_half col_last">
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> (212) 8547 632521<br>
+									<abbr title="Fax"><strong>Fax:</strong></abbr> (212) 11 4752 1433<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> Ensem@gamil.com
+								</div>
+							</div>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-twitter">
+								<i class="icon-twitter"></i>
+								<i class="icon-twitter"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-gplus">
+								<i class="icon-gplus"></i>
+								<i class="icon-gplus"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-pinterest">
+								<i class="icon-pinterest"></i>
+								<i class="icon-pinterest"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-vimeo">
+								<i class="icon-vimeo"></i>
+								<i class="icon-vimeo"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-yahoo">
+								<i class="icon-yahoo"></i>
+								<i class="icon-yahoo"></i>
+							</a>
+
+							<a href="#" class="social-icon si-small si-rounded topmargin-sm si-linkedin">
+								<i class="icon-linkedin"></i>
+								<i class="icon-linkedin"></i>
+							</a>
+
+						</div>
+
+					</div>
+
+					<div class="col_one_fourth">
+
+						<div class="widget clearfix">
+							<h4>Articles récents</h4>
+
+							<div id="post-list-footer">
+								<div class="spost clearfix">
+									<div class="entry-image d-md-none d-lg-block">
+										<a href="#"><img class="rounded-circle" src="includes/images/small/1.jpg" alt=""></a>
+									</div>
+									<div class="entry-c">
+										<div class="entry-title">
+											<h4><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h4>
+										</div>
+										<ul class="entry-meta">
+											<li>10th July 2014</li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="spost clearfix">
+									<div class="entry-image d-md-none d-lg-block">
+										<a href="#"><img class="rounded-circle" src="includes/images/small/2.jpg" alt=""></a>
+									</div>
+									<div class="entry-c">
+										<div class="entry-title">
+											<h4><a href="#">Elit Assumenda vel amet dolorum quasi</a></h4>
+										</div>
+										<ul class="entry-meta">
+											<li>10th July 2014</li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="spost clearfix">
+									<div class="entry-image d-md-none d-lg-block">
+										<a href="#"><img class="rounded-circle" src="includes/images/small/3.jpg" alt=""></a>
+									</div>
+									<div class="entry-c">
+										<div class="entry-title">
+											<h4><a href="#">Debitis nihil placeat, illum est nisi</a></h4>
+										</div>
+										<ul class="entry-meta">
+											<li>10th July 2014</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="col_one_fourth col_last">
+
+						<div class="widget quick-contact-widget form-widget clearfix">
+
+							<h4>ENVOYER LE MESSAGE</h4>
+
+							<div class="form-result"></div>
+
+							<form id="quick-contact-form" name="quick-contact-form" action="include/form.php" method="post" class="quick-contact-form nobottommargin">
+
+								<div class="form-process"></div>
+
+								<div class="input-group divcenter">
+									<div class="input-group-prepend">
+										<div class="input-group-text"><i class="icon-user"></i></div>
+									</div>
+									<input type="text" class="required form-control" id="quick-contact-form-name" name="quick-contact-form-name" value="" placeholder="Full Name" />
+								</div>
+								<div class="input-group divcenter">
+									<div class="input-group-prepend">
+										<div class="input-group-text"><i class="icon-email2"></i></div>
+									</div>
+									<input type="text" class="required form-control email" id="quick-contact-form-email" name="quick-contact-form-email" value="" placeholder="Email Address" />
+								</div>
+								<textarea class="required form-control input-block-level short-textarea" id="quick-contact-form-message" name="quick-contact-form-message" rows="4" cols="30" placeholder="Message"></textarea>
+								<input type="text" class="hidden" id="quick-contact-form-botcheck" name="quick-contact-form-botcheck" value="" />
+								<input type="hidden" name="prefix" value="quick-contact-form-">
+								<button type="submit" id="quick-contact-form-submit" name="quick-contact-form-submit" class="btn sendButton nomargin" value="submit">Envoyer un e-mail</button>
+
+							</form>
+
+						</div>
+
+					</div>
+
+				</div><!-- .footer-widgets-wrap end -->
+
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_full nobottommargin center">
+						<div class="copyrights-menu copyright-links clearfix">
+							<a href="#">Acuil</a>/<a href="#">L'ecole</a>/<a href="#">Espace-Etudiant</a>/<a href="#">Espace-Entreprise</a>/<a href="#">Contact</a>
+						</div>
+						ENSEM &copy; 2020 Tous les droits sont réservés.
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+		<!-- Go To Top
+			============================================= -->
+			<div id="gotoTop" class="icon-angle-up"></div>
+
+
+</html>
